a0b4b640f920ee6449efb493019a7d4b5e3fa33b
walaa-horani <59100447+walaa-horani@users.noreply.github.com>
2022-06-20 12:04:22 +0300

    Update settings.py

--- photo_gellery/settings.py
+++ photo_gellery/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-y@x7w$#h=)q+wvh9u1-htkp#a%iu%ibpr4%lnpy)0$thi$ccxs
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1','galleryblog.herokuapp.com']
+ALLOWED_HOSTS = ['','']
 
 
 # Application definition
